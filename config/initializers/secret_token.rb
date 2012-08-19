d05f0cd8a5932b05876afa3497fd382e64977e0b
Jay Earwood <jay.earwood@gmail.com>
2012-08-19 11:33:54 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShorten::Application.config.secret_token = 'd56984d29aac812e3773c884110b06e52adf7162e76f108629aff482bf2d8b54f63ef7051daf8cfb411be2160d11c053397aee96a17d94068becbdd8461906f2'
