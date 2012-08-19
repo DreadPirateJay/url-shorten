45d8cbd9ad232a0b607dddc6bd5e2ff592792264
Jay Earwood <jay.earwood@gmail.com>
2012-08-19 13:12:46 -0500

    Added test for urls_controller :create

--- test/functional/urls_controller_test.rb
+++ test/functional/urls_controller_test.rb
@@ -5,4 +5,9 @@ class UrlsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+  
+  test "should create shortened url id" do
+    post :create, :url => { 'url' => 'http://www.google.com' }
+    assert_not_nil(flash[:shortened_id])
+  end
 end
