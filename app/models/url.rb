f8b26f1d1a8ad01c4cb494fcfb4216e3ba19a293
Jay Earwood <jay.earwood@gmail.com>
2012-08-19 13:42:27 -0500

    Added validation for properly formatted urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,5 @@
 class Url < ActiveRecord::Base
   attr_accessible :url
   validates :url, :presence => true
+  validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 end
